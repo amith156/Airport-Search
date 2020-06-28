370139c70c1e51866ade67ccbdd97ab122faaa84
Amith's MacBook <amithkumarnarayan1@gmail.com>
2020-06-28 17:22:06 +0100

    Initial Commit

---
+++ AirportSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirportSearch
+//
+//  Created by Amith Kumar Narayan on 28/06/2020.
+//  Copyright © 2020 Narayan inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
