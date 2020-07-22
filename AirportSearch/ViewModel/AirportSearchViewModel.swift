cefc346c7ce1bb37d16fdf58100ca75196107599
Amith's MacBook <amithkumarnarayan1@gmail.com>
2020-07-22 22:39:00 +0100

    fires RxSwift with coldObservable and all the required setup with the RxSwift operators

--- AirportSearch/ViewModel/AirportSearchViewModel.swift
+++ AirportSearch/ViewModel/AirportSearchViewModel.swift
@@ -23,16 +23,54 @@ class AirportSearchViewModel : AirportSearchViewProtocol {
     
     init(input : AirportSearchViewProtocol.Input, airportService : AirportService) {
         self.input = input
-        self.output = AirportSearchViewModel.output(input: self.input)
+        self.output = AirportSearchViewModel.output(input: self.input,
+                                                    state: self.state,
+                                                    bag: self.bag)
         self.airportService = airportService
         processData()
     }
 }
 
 private extension AirportSearchViewModel {
-    static func output(input : AirportSearchViewProtocol.Input) -> AirportSearchViewProtocol.Output {
+    static func output(input : AirportSearchViewProtocol.Input,
+                       state : State,
+                       bag : DisposeBag) -> AirportSearchViewProtocol.Output {
         
-        return ()
+        let searchTextObservable = input.searchText
+            .debounce(.milliseconds(350))
+            .distinctUntilChanged()
+            .skip(1)
+            .asObservable()
+            .share(replay: 1, scope: .whileConnected)
+        
+        let airportStateObservable = state.airports
+            .skip(1)
+            .asObservable()
+        
+        searchTextObservable.map({ (val) in
+            print("=====> \(val)")
+            
+        }).subscribe()
+            .disposed(by: bag)
+        
+        Observable
+            .combineLatest(searchTextObservable, airportStateObservable)
+            .map ({ (searchKey, airportSet) in
+                return airportSet.filter { (airport) -> Bool in
+                    guard let city = airport.city else { return false }
+                    return !searchKey.isEmpty &&
+                        city
+                            .lowercased()
+                            .replacingOccurrences(of: " ", with: "")
+                            .hasPrefix(searchKey.lowercased())
+                    
+                }
+            })
+            .map{
+                print($0,"\n")
+            }
+            .subscribe()
+            .disposed(by: bag)
     }
     
     func processData() {
